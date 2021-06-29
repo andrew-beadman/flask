b43b79baa210eb6d2425bceeb27571bd98369a7e
andrew-beadman <83642014+andrew-beadman@users.noreply.github.com>
2021-06-29 10:18:12 +0100

    Create __main__.py

---
+++ srv/__main__.py
@@ -0,0 +1,21 @@
+from flask import Flask, json, render_template, session
+from flaskwebgui import FlaskUI
+
+
+app = Flask(__name__)
+ui = FlaskUI(app, port=5000, width=600, height=500)
+app.config["DEBUG"] = os.environ.get("FLASK_DEBUG", True)
+app.config["JSON_AS_ASCII"] = False
+app.config['SECRET_KEY'] = 'dev'
+app.config.from_mapping(
+    BASE_URL="http://localhost:5000",
+)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    # app.run() for debug
+    ui.run()
